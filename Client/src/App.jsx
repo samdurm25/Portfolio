02edd5f3af8bcad05caf53ace8cb9ba45758d56f
Sam Durm <samdurm@outlook.com>
2024-10-11 13:00:41 -0400

    Making location changes on desktop pc

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>Vite/React</p>
+    </>
+  )
+}
+
+export default App
