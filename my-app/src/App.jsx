50a15a3f936627b1989e211a09821ef21bf52971
Sam Durm <samdurm@outlook.com>
2024-10-11 17:39:35 -0400

    Creaqting header

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <> 
-      
+      <Header></Header>
     </>
   )
 }
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Sam Durm</h1>
+        </div>
+    );
+};
+
+export default Header; 
