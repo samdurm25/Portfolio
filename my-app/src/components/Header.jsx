904a38b1b58248e314fce4d10f52f1e7092ad5d5
Durm, Samuel J <samdurm@outlook.com>
2024-10-11 19:01:52 -0400

    Trying to add navigation with react scroll

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,11 +1,29 @@
+import { Link } from "react-scroll";
 
 const Header = () => {
-    return (
-        <div className="h-screen font-sans">
-            <h1>Sam</h1>
-            <h1>Durm</h1>
-        </div>
-    );
+	return (
+		<div className="flex justify-between">
+			<div className="h-screen font-sans">
+				<h1>Sam</h1>
+				<h1>Durm</h1>
+			</div>
+
+			<ul className="flex-col">
+				<Link spy={true} smooth={true} to="Home">
+					<li>Home</li>
+				</Link>
+				<Link spy={true} smooth={true} to="About">
+					<li>About</li>
+				</Link>
+				<Link spy={true} smooth={true} to="Projects">
+					<li>Projects</li>
+				</Link>
+				<Link spy={true} smooth={true} to="Contact">
+					<li>Contact</li>
+				</Link>
+			</ul>
+		</div>
+	);
 };
 
 export default Header; 
